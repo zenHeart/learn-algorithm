2db0a6859852166f5f281e74e09785eb98edd03f
zenheart <zenheart_register@163.com>
2025-11-23 11:51:02 +0800

    fix: 添加矩形面积计算题目

--- docs/leetcode/223.rectangle-area/index.js
+++ docs/leetcode/223.rectangle-area/index.js
@@ -9,8 +9,15 @@
  * @param {number} by2
  * @return {number}
  */
-var computeArea = function(ax1, ay1, ax2, ay2, bx1, by1, bx2, by2) {
-    
+var computeArea = function (ax1, ay1, ax2, ay2, bx1, by1, bx2, by2) {
+   const sr1 = (ax2 - ax1) * (ay2 - ay1)
+   const sr2 = (bx2 - bx1) * (by2 - by1)
+   let acrossX = Math.min(ax2, bx2) - Math.max(ax1, bx1)
+   acrossX = acrossX > 0 ? acrossX : 0;
+   let acrossY = Math.min(ay2, by2) - Math.max(by1, ay1)
+   acrossY = acrossY > 0 ? acrossY : 0;
+   const overRec = acrossX * acrossY
+   return sr1 + sr2 - overRec
 };
 
 export default computeArea;
--- docs/leetcode/223.rectangle-area/index.test.js
+++ docs/leetcode/223.rectangle-area/index.test.js
@@ -2,13 +2,21 @@ import { describe, it, expect } from 'vitest';
 import computeArea from './index';
 
 let testData = {
-  "示例 1": {
-    input: [-3,0,3,4,0,-1,9,2],
-    expect: 45
+  "两个矩形部分重叠": {
+    input: [-3, 0, 3, 4, 0, -1, 9, 2],
+    expect: 45,
   },
-  "示例 2": {
-    input: [-2,-2,2,2,-2,-2,2,2],
-    expect: 16
+  "两个矩形完全重合": {
+    input: [-2, -2, 2, 2, -2, -2, 2, 2],
+    expect: 16,
+  },
+  "两个矩形不重叠": {
+    input: [0, 0, 1, 1, 2, 2, 3, 3],
+    expect: 2,
+  },
+  "一个矩形包含另一个矩形": {
+    input: [0, 0, 4, 4, 1, 1, 2, 2],
+    expect: 16,
   },
 };
 
